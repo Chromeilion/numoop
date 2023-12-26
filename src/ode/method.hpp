b3e0d70e4acdc60cdd1267d3f2b8216f35257cfe
CSpringe20 <christian.springer228@gmail.com>
2023-12-26 22:13:17 +0100

    a

--- src/ode/method.hpp
+++ src/ode/method.hpp
@@ -14,16 +14,14 @@ class ODE {
 public:
     virtual ~ODE() = default;
 
-    // Virtual method
-    virtual arma::mat method(const std::function<arma::vec(double, const arma::vec&)>& f, arma::mat& result_mat, const arma::vec& t, double h) const = 0;
-
-    // Method implementation
-    arma::mat ode(const std::function<arma::vec(double, const arma::vec&)>& f, const arma::vec& y0, double h, double end, const std::string& filename = "result.csv") {
+    // Templated method for generic types
+    template <typename T>
+    arma::mat ode(const std::function<arma::vec(double, const arma::vec&)>& f, const T& y0, double h, double end, const std::string& filename = "result.csv") {
         arma::uword steps = static_cast<arma::uword>(end / h) + 1;
         arma::vec t = arma::linspace(0, end, steps);
 
         arma::mat result_mat(y0.n_elem, steps);
-        result_mat.col(0) = y0;
+        result_mat.col(0) = arma::conv_to<arma::vec>::from(y0);
 
         // Call the virtual method
         result_mat = method(f, result_mat, t, h);
@@ -35,10 +33,21 @@ public:
         result_mat.insert_rows(0, t.t());
 
         // Save the result to the specified or default CSV file with a header
+        saveToFile(result_mat, t, filename);
+
+        return out;
+    }
+
+    // Virtual method
+    virtual arma::mat method(const std::function<arma::vec(double, const arma::vec&)>& f, arma::mat& result_mat, const arma::vec& t, double h) const = 0;
+
+private:
+    // Save results to file
+    void saveToFile(const arma::mat& result_mat, const arma::vec& t, const std::string& filename) const {
         std::ofstream file(filename);
         if (file.is_open()) {
             file << "t,y0";
-            for (arma::uword i = 1; i < y0.n_elem; ++i) {
+            for (arma::uword i = 1; i < result_mat.n_rows; ++i) {
                 file << ",y" << i;
             }
             file << "\n";
@@ -48,9 +57,7 @@ public:
         } else {
             std::cerr << "Error: Unable to open file for writing.\n";
         }
-
-        return out;
     }
 };
 
-#endif //METHOD_HPP
+#endif // METHOD_HPP

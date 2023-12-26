42fcb75a507953d5d92803bc9e88c5223423ec29
CSpringe20 <christian.springer228@gmail.com>
2023-12-26 22:17:21 +0100

    a

--- src/ode/method.hpp
+++ src/ode/method.hpp
@@ -15,8 +15,8 @@ public:
     virtual ~ODE() = default;
 
     // Templated method for generic types
-    template <typename T>
-    arma::mat ode(const std::function<arma::vec(double, const arma::vec&)>& f, const T& y0, double h, double end, const std::string& filename = "result.csv") {
+    template <typename YType>
+    arma::mat ode(const std::function<arma::vec(double, const arma::vec&)>& f, const YType& y0, double h, double end, const std::string& filename = "result.csv") {
         arma::uword steps = static_cast<arma::uword>(end / h) + 1;
         arma::vec t = arma::linspace(0, end, steps);
 
@@ -33,21 +33,10 @@ public:
         result_mat.insert_rows(0, t.t());
 
         // Save the result to the specified or default CSV file with a header
-        saveToFile(result_mat, filename);
-
-        return out;
-    }
-
-    // Virtual method
-    virtual arma::mat method(const std::function<arma::vec(double, const arma::vec&)>& f, arma::mat& result_mat, const arma::vec& t, double h) const = 0;
-
-private:
-    // Save results to file
-    void saveToFile(const arma::mat& result_mat, const std::string& filename) const {
         std::ofstream file(filename);
         if (file.is_open()) {
             file << "t,y0";
-            for (arma::uword i = 1; i < result_mat.n_rows; ++i) {
+            for (arma::uword i = 1; i < y0.n_elem; ++i) {
                 file << ",y" << i;
             }
             file << "\n";
@@ -57,7 +46,9 @@ private:
         } else {
             std::cerr << "Error: Unable to open file for writing.\n";
         }
+
+        return out;
     }
 };
 
-#endif // METHOD_HPP
+#endif //METHOD_HPP

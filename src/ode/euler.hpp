07fdae4fa69e19cbaffa4551e21dc92599d67683
CSpringe20 <christian.springer228@gmail.com>
2023-12-26 20:19:38 +0100

    a

--- src/ode/euler.hpp
+++ src/ode/euler.hpp
@@ -9,9 +9,8 @@
 
 class Euler : public ODE {
 public:
-    template <typename T1, typename T2>
     // Implement Euler method
-    arma::mat method(const std::function<arma::vec(T1, const T2&)>& f, arma::mat& result_mat, const arma::vec& t, double h) const override {
+    arma::mat method(const std::function<arma::vec(double, const arma::vec&)>& f, arma::mat& result_mat, const arma::vec& t, double h) const override {
         for (arma::uword i = 1; i < t.n_elem; ++i) {
             result_mat.col(i) = result_mat.col(i - 1) + h * f(t(i), result_mat.col(i - 1));
         }

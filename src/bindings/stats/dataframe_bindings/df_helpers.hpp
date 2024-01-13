fab839ca8edf5a440da184f48a12060242972510
Chromeilion <uki@cro.moe>
2024-01-13 13:55:08 +0100

    fix view function

--- src/bindings/stats/dataframe_bindings/df_helpers.hpp
+++ src/bindings/stats/dataframe_bindings/df_helpers.hpp
@@ -11,6 +11,26 @@ namespace oop::stats::bindings {
     class ConvertArray;
     sup_arr_types convert_col(oop::stats::sup_col_types &col);
     oop::stats::sup_col_types transfer_col_ownership(sup_arr_types &col);
+    // Functor for creating a const numpy view into an arma array.
+    class ToNumpyView {
+    public:
+        template<typename T>
+        inline sup_arr_types operator()(const T &col) const {
+            const auto view = carma::to_numpy_view<T>(col);
+            const sup_arr_types conv{view};
+            return conv;
+        }
+    };
+    // Functor for getting an element from a DataFrame with an int or str index.
+    class GetIdx {
+        const oop::stats::DataFrame& self;
+    public:
+        inline explicit GetIdx(const oop::stats::DataFrame& self_) : self{self_} {};
+        inline const oop::stats::sup_col_types& operator()(const unsigned &idx)
+        const {return self[idx];}
+        inline const oop::stats::sup_col_types& operator()(const std::string &lab)
+        const {return self(lab);}
+    };
 }
 
 #endif //NUMOOP_DF_HELPERS_HPP
--- src/bindings/stats/dataframe_bindings/df_view.cpp
+++ src/bindings/stats/dataframe_bindings/df_view.cpp
@@ -2,26 +2,22 @@
 // Created by uki on 1/6/24.
 //
 #include "dataframe_bindings.hpp"
+#include "df_helpers.hpp"
 #include <variant>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <carma>
 
 namespace py=pybind11;
 namespace oop::stats::bindings {
     sup_arr_types view_wrapper(const oop::stats::DataFrame &self,
                                const std::variant<unsigned, std::string> &idx) {
-        const auto &col = std::visit(overloaded{
-            [&self](const unsigned &index){return self[index];},
-            [&self](const std::string &lab){return self(lab);}
-            }, idx);
-        auto to_view_wrap = [](auto &c)
-                {return sup_arr_types{carma::to_numpy_view(c)};};
-        return std::visit(to_view_wrap, col);
+        const auto &col = std::visit(GetIdx(self), idx);
+        const sup_arr_types conv{std::visit(ToNumpyView(), col)};
+        return conv;
     }
 
     void df_view(py::class_<oop::stats::DataFrame> &m) {
-        m.def("view", view_wrapper,
+        m.def("view", &view_wrapper,
               py::arg("idx"),
               R"pbdoc(
 Get a view into a column. This is much faster than indexing the dataframe
@@ -34,7 +30,7 @@ idx : unsigned int or string
 
 Returns
 -------
-col : np.typing.NDArray
+col : np.typing.NDArray[sup_col_types]
     A read-only numpy array.
 )pbdoc");
     }
